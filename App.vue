3bf701d2e52736d6ed88c759c01ddb1f3d999e7b
wl121470 <m15718608738@163.com>
2019-10-16 16:20:55 +0800

    wl

--- App.vue
+++ App.vue
@@ -2,13 +2,13 @@
 	import "@/common/index.css"
 	export default {
 		onLaunch: function() {
-			console.log("打虎上山")
+			console.log("打虎上山1111")
 		},
 		onShow: function() {
-			console.log('阿斯顿撒奥多所')
+			console.log('阿斯顿撒奥多所111')
 		},
 		onHide: function() {
-			console.log('爱仕达多所')
+			console.log('爱仕达多所111')
 		}
 	}
 </script>
