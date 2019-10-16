7a6ec3172bc16b2c10cc9241bb472e8ec2a2d988
hqj <754546313@qq.com>
2019-10-16 18:44:08 +0800

    hqj

--- App.vue
+++ App.vue
@@ -2,17 +2,19 @@
 	import "@/common/index.css"
 	export default {
 		onLaunch: function() {
-			console.log("进入的时候输入一次")
+			
 		},
 		onShow: function() {
-			console.log('每次打开页面显示加载')
+			
 		},
 		onHide: function() {
-			console.log('每次页面消失显示加载')
+			
 		}
 	}
 </script>
-<style>/
+
+<style>
+	/*每个页面公共css */
 *{
 	font-size: 28upx;
 }
