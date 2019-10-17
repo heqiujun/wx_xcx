41899e95927aff18290d59c5d0d2cfbccf86a8b2
hqj <754546313@qq.com>
2019-10-17 08:55:14 +0800

    hqj

--- App.vue
+++ App.vue
@@ -2,17 +2,19 @@
 	import "@/common/index.css"
 	export default {
 		onLaunch: function() {
+			console.log("第一次进入小程序")
 			
 		},
 		onShow: function() {
+			console.log("每次进入这个页面")
 			
 		},
 		onHide: function() {
+			console.log("每次从这个页面消失")
 			
 		}
 	}
 </script>
-
 <style>
 	/*每个页面公共css */
 *{
